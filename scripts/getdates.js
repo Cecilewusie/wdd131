908f89c0518fbc643a5f0ad4a35ba7b98e86fbcc
Cecilewusie <ewu22001@byui.edu>
2024-09-24 23:08:31 +0000

    added a feature to track remaining days to my birthday

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,11 @@ year.innerHTML = `${todaysDate.getFullYear()}`;
 
 const modified = document.querySelector("#lastModified");
 modified.innerHTML = `Last date page was modified ${document.lastModified}`;
+
+const Remaining = document.querySelector("#monthsRemaining");
+const birthmonth = 9;
+if (birthmonth > todaysDate.getMonth()) {
+    Remaining.innerHTML = `${birthmonth-todaysDate.getMonth()}`;
+} else {
+    Remaining.innerHTML = `${todaysDate.getMonth() - birthmonth}`
+};
