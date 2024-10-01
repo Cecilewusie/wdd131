1e0228077ea91235541d0aff514ec5010c6bc7d1
Cecilewusie <ewu22001@byui.edu>
2024-10-01 00:26:38 +0000

    Done with the book of Mormon

--- week02/scripts/bomscript.js
+++ week02/scripts/bomscript.js
@@ -3,20 +3,37 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-//Creating a li element that will hold each entries chapter title and an associated delete button.
-const liElement = document.createElement("li");
 
-//Creating a delete button.
-const deleteButton = document.createElement("button");
 
-//Populating the li element variable's textContent or innerHTML with the input value.
-liElement.textContent = input.value;
+button.addEventListener("click", function () {
+    if(input.value.trim() !== " ") {
+        //Creating a li element that will hold each entries chapter title and an associated delete button.
+        const liElement = document.createElement("li");
 
-//Populating the button textContent with a ❌.
-button.textContent = "❌";
+        //Creating a delete button.
+        const deleteButton = document.createElement("button");
 
-//Appending the li element variable with the delete button.
-liElement.append(deleteButton);
+        //Populating the li element variable's textContent or innerHTML with the input value.
+        liElement.textContent = input.value;
 
-//Appending the li element variable to the unordered list in your HTML
-list.append(liElement);
+        //Populating the button textContent with a ❌.
+        deleteButton.textContent = "❌";
+
+        //Appending the li element variable with the delete button.
+        liElement.append(deleteButton);
+
+        //Appending the li element variable to the unordered list in your HTML
+        list.append(liElement);
+
+        //Adding an event listener to the delete button that removes the li element when clicked
+        deleteButton.addEventListener("click", function() {
+            list.removeChild("li");
+            input.focus();
+        });
+    }
+
+    else {
+        input.value === "";
+        input.focus();
+    }
+})
